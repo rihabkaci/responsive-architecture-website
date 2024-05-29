ff46a204389f528d27be2d62b5a53ecf127c602a
rihabkaci <rihabk99@gmail.com>
2024-05-29 15:24:46 +0100

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,51 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+//show menu
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+// hide menu
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
-
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== SWIPER HOMES ===============*/
+const swiperHome = new Swiper(".home__swiper", {
+  loop: true,
+  speed: 800,
+  parallax: true,
+  effect: "fade",
 
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+
+    formatFractionCurrent: (number) => {
+      return "0" + number;
+    },
+    formatFractionTotal: (number) => {
+      return "0" + number;
+    },
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
